9f8a9fe5a39648c9bb2f3275a6733676f7ee1ffd
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-08-13 14:56:58 +0300

    add copy card and ****

--- src/components/CardElement/CardElement.jsx
+++ src/components/CardElement/CardElement.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useDeleteCardsMutation } from 'redux/walletApi';
 import { AddCard } from 'components/AddCard/AddCard';
 import s from './CardElement.module.css';
@@ -16,6 +16,23 @@ export const CardElement = props => {
   } = props;
   const [deleteCard] = useDeleteCardsMutation();
   const [showEditCard, setShowAddCard] = useState(false);
+  const [showNumberCard, setShowNumberCard] = useState(false);
+  const isHidenCardNumber = useMemo(() => {
+    return cardNumber
+      .split('-')
+      .map((el, idx) => {
+        if (idx === 1 || idx === 2) {
+          return '****';
+        } else {
+          return el;
+        }
+      })
+      .join('-');
+  }, [cardNumber]);
+
+  const handleShowNumber = () => {
+    setShowNumberCard(prev => !prev);
+  };
 
   const handleTogleEditCard = () => {
     setShowAddCard(prev => !prev);
@@ -36,9 +53,18 @@ export const CardElement = props => {
               <p>{cardType}</p>
             </div>
           </div>
-          <div>
-            <p>{cardNumber}</p>
-            <button>copy</button>
+          <div className={s.cardNum}>
+            <p onClick={handleShowNumber}>
+              {showNumberCard ? cardNumber : isHidenCardNumber}
+            </p>
+            <button
+              type="button"
+              onClick={() => {
+                navigator.clipboard.writeText(cardNumber);
+              }}
+            >
+              copy
+            </button>
           </div>
           <div>
             <p>{expDate}</p>
